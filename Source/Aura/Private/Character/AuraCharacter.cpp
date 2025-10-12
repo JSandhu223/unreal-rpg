8a4127a311f922acb0e7d2196701af4358f3d880
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-10-11 22:28:37 -0600

    Configured movement component on Aura character to ensure movement constraints and to prevent camera rotation.

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,18 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+
+AAuraCharacter::AAuraCharacter()
+{
+	UCharacterMovementComponent* MovementComp = this->GetCharacterMovement();
+	MovementComp->bOrientRotationToMovement = true;
+	MovementComp->RotationRate = FRotator(0.0f, 400.0f, 0.0f);
+	MovementComp->bConstrainToPlane = true;
+	MovementComp->bSnapToPlaneAtStart = true;
+
+	this->bUseControllerRotationPitch = false;
+	this->bUseControllerRotationRoll = false;
+	this->bUseControllerRotationYaw = false;
+}
