4a996c77bd351d1639ecc285a22c6048d089f5de
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-10-13 14:27:29 -0600

    Now highlights weapon attached to enemy. Stencil value is now a macro defined in Aura.h

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,15 +2,19 @@
 
 
 #include "Character/AuraEnemy.h"
+#include "Aura/Aura.h"
 
 
 void AAuraEnemy::HighlightActor()
 {
 	this->GetMesh()->SetRenderCustomDepth(true);
-	this->GetMesh()->SetCustomDepthStencilValue(this->StencilValue);
+	this->GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
 	this->GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
 }
