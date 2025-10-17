93388fed30663ad95072800da5019fcb477916cc
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-10-16 18:50:48 -0600

    In the character base class, created pointers to an AbilitySystemComponent and AttributeSet. Initialized these fields in the AuraEnemy constructor.

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,12 +3,21 @@
 
 #include "Character/AuraEnemy.h"
 #include "Aura/Aura.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
 
 
 AAuraEnemy::AAuraEnemy()
 {
 	// Note that this will set the 'Collision Preset' in the editor to 'Custom'
 	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Visibility, ECollisionResponse::ECR_Block);
+
+	// The ability system component will exist on all enemies
+	this->AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	this->AbilitySystemComponent->SetIsReplicated(true);
+
+	// The ability system component will exist on all enemies
+	this->AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 }
 
 void AAuraEnemy::HighlightActor()
--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -6,6 +6,11 @@
 #include "GameFramework/Character.h"
 #include "AuraCharacterBase.generated.h"
 
+
+class UAbilitySystemComponent;
+class UAttributeSet;
+
+
 UCLASS(Abstract)
 class AURA_API AAuraCharacterBase : public ACharacter
 {
@@ -21,4 +26,10 @@ protected:
 
 	UPROPERTY(EditAnywhere, Category = "Combat")
 	TObjectPtr<USkeletalMeshComponent> Weapon;
+
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+	
+	UPROPERTY()
+	TObjectPtr<UAttributeSet> AttributeSet;
 };
