206f3f78859ef21b7d4bccb659de93c51bc747d4
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-10-20 20:43:41 -0600

    On sphere overlap now adds 25 health. This is just for experimenting with the health attribute and will be changed later to incorporate gameplay effects. Created health potion blueprint derived from AuraEffectActor to test health attribute updating.

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -3,6 +3,9 @@
 
 #include "Actor/AuraEffectActor.h"
 #include "Components/SphereComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
 
 
 // Sets default values
@@ -27,6 +30,18 @@ void AAuraEffectActor::BeginPlay()
 
 void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	// TODO: Change this to apply a Gameplay Effect. For now, we're using const_cast as a hack!
+	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+
+		if (AuraAttributeSet)
+		{
+			UAuraAttributeSet* MutableAuraAttributeSet = const_cast<UAuraAttributeSet*>(AuraAttributeSet);
+			MutableAuraAttributeSet->SetHealth(AuraAttributeSet->GetHealth() + 25.0f);
+			this->Destroy();
+		}
+	}
 }
 
 void AAuraEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
