33bf0aa8bf05708f78a9b91b08276d99d9c2d180
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-11-09 19:23:21 -0700

    Created gameplay tags for messages related to our vital attributes. Created data table asset for messages related to widget data (health, mana). Added field for a UDataTable pointer to OverlayWidgetController.

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -64,11 +64,11 @@ public:
 	FOnMaxManaChangedSignature OnMaxManaChanged;
 
 protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widget Data")
+	TObjectPtr<UDataTable> MessageWidgetDataTable;
+
 	void HealthChanged(const FOnAttributeChangeData& Data) const;
-
 	void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
-
 	void ManaChanged(const FOnAttributeChangeData& Data) const;
-	
 	void MaxManaChanged(const FOnAttributeChangeData& Data) const;
 };
