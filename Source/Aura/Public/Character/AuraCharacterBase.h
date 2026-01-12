554104173572649a5f980b778c4c559e47948736
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2026-01-11 21:33:26 -0700

    Created interface to be used for combat-related functions by both the player and enemy. This interface is implemented by AuraCharacterBase. Created a Level field and added override for it's getter function on the player state and enemy.

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -3,8 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AbilitySystemInterface.h"
 #include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "Interaction/CombatInterface.h"
 #include "AuraCharacterBase.generated.h"
 
 
@@ -14,7 +15,7 @@ class UGameplayEffect;
 
 
 UCLASS(Abstract)
-class AURA_API AAuraCharacterBase : public ACharacter, public IAbilitySystemInterface
+class AURA_API AAuraCharacterBase : public ACharacter, public IAbilitySystemInterface, public ICombatInterface
 {
 	GENERATED_BODY()
 
